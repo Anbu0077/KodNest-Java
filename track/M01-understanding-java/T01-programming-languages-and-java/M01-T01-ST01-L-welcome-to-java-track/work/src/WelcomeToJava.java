dcd4f5d8cc7d9f6b6c9fa5e814804b8b30fe4ee6
Anburaj <anbu88628@gmail.com>
2026-07-30 12:50:47 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,7 +3,8 @@ public class WelcomeToJava {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Java Track");
-        System.out.println("I already know how to build programming logic.");
-        System.out.println("Now I am learning how to express that logic using java.");
+        System.out.println("My name is Anbu.");
+        System.out.println("My goal is to become a java Developer.");
+        System.out.println("I selected java because I want to build large applications.");
     }
 }
