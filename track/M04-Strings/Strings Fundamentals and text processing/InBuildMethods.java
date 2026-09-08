3135ab203eb0f237c4edbebdeaa08c9a1437bed8
Anburaj <anbu88628@gmail.com>
2026-09-08 16:53:41 +0530

    InBuildMethod

---
+++ track/M04-Strings/Strings Fundamentals and text processing/InBuildMethods.java
@@ -0,0 +1,24 @@
+
+public class InBuildMethods {
+
+    public static void main(String args[]) {
+        String s1 = "Java";
+        String s2 = "JavA";
+        if (s1 == s2) {
+            System.out.println("Ref are same");
+        } else {
+            System.out.println("Ref are not same");
+        }
+        if (s1.equals(s2)) {
+            System.out.println("Strings are same");
+        } else {
+            System.out.println("Strings are not same");
+        }
+        if (s1.equalsIgnoreCase(s2)) {
+            System.out.println("Strings are same");
+        } else {
+            System.out.println("Strings are not same");
+        }
+
+    }
+}
